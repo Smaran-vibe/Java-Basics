a85359c45a1139ae188fa0ba1daddcc44900238e
Smaran <smaranaryal60@gmail.com>
2026-02-06 20:47:42 +0545

    Written some basics programs and fixed the logical error

--- Strings/firs_letter_to_uppercase.java
+++ Strings/firs_letter_to_uppercase.java
@@ -11,19 +11,16 @@ public class firs_letter_to_uppercase {
             System.out.println("Enter the text: ");
             String text = input.nextLine();
 
-            // use substring() to split the input string to two strings
-            // first substring contains the first letter
-            // hint: to access first letter, use indexes 0 and 1
+
             String firstLetter = text.substring(0,1);
 
-            // second substring contains the remaining letters
-            // hint: to use remaining letters, use indexes 1 and string.length()
+
             String remainingLetters = text.substring(1);
 
-            // change the first letter to uppercase
+
             firstLetter = firstLetter.toUpperCase();
 
-            // join the two substrings
+
             text = firstLetter + remainingLetters;
             System.out.println(text);
 
