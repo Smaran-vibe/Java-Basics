ac9a7205dc9dd0834b63e997badb1f1249adc856
Smaran <smaranaryal60@gmail.com>
2026-02-27 21:00:21 +0545

    use of static

---
+++ OOPS/sta_Tic.java
@@ -0,0 +1,20 @@
+package OOPS;
+class ani_mal {
+
+
+    static void display() {
+        System.out.println("I am an animal.");
+    }
+
+}
+
+class sta_Tic{
+
+
+    public static void main(String[] args) {
+
+
+        ani_mal.display();
+    }
+}
+
